8b94b1ba6368d03b729e8e6b849ba354994cbf81
Builder <92662457+Builder256@users.noreply.github.com>
2025-08-09 19:17:30 +0900

    画像の拡大プレビュー機能を実装

--- assets/scripts/body.js
+++ assets/scripts/body.js
@@ -10,6 +10,7 @@ window.boot.register('page-ready', () => {
     openExternalLinkInNewTab();
     changeHeadingLinkIcons();
     convertImagesToFigures();
+    addImagePreview();
 });
 
 /**
@@ -310,5 +311,56 @@ function convertImagesToFigures() {
             div.appendChild(figure);
         }
         filteredParagraph.replaceWith(div);
+    }
+}
+
+/**
+ * 画像のモーダルプレビューを実装
+ * @returns {void}
+ */
+function addImagePreview() {
+    /**
+     * @type {NodeListOf<HTMLImageElement>}
+     */
+    const images = document.querySelectorAll('.contents img');
+
+    const insertDialogElement = () => {
+        const dialog = document.createElement('dialog');
+        const menu = document.createElement('menu');
+        const closeLi = document.createElement('li');
+        const newTabLi = document.createElement('li');
+        const closeButton = document.createElement('button');
+        const newTabButton = document.createElement('button');
+        const previewImage = document.createElement('img');
+
+        closeButton.textContent = '閉じる';
+        closeButton.setAttribute('autofocus', '');
+        closeButton.addEventListener('click', () => {
+            dialog.close();
+        });
+        closeLi.appendChild(closeButton);
+
+        newTabButton.textContent = '画像を新しいタブで開く';
+        newTabButton.addEventListener('click', () => {
+            window.open(previewImage.src);
+        });
+        newTabLi.appendChild(newTabButton);
+        menu.append(closeLi, newTabLi);
+        dialog.append(menu, previewImage);
+        document.body.appendChild(dialog);
+        return dialog;
+    };
+
+    const dialog = insertDialogElement();
+    for (const image of images) {
+        image.addEventListener('click', () => {
+            const { src, alt } = image;
+            const targetImage = dialog.querySelector('img');
+            if (!targetImage) return;
+            targetImage.src = src;
+            targetImage.alt = alt;
+
+            dialog.showModal();
+        });
     }
 }
