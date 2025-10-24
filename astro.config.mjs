c402a0172c1a00793fe25edc7abb1a3bcb6d22c6
Builder <92662457+Builder256@users.noreply.github.com>
2025-10-24 20:03:04 +0900

    コードフォーマット

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,186 +13,189 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   site: 'https://builder256.github.io',
   base: 'RTM-Wiki',
-  integrations: [starlight({
-    title: 'RTM Wiki',
-    description:
-      '2025年に新しく設立されたRealTrainMod（RTM）のWiki、中級者以上のユーザーに向けた高度な情報を掲載',
-    logo: {
-      src: './src/assets/images/logos/wiki-logo.svg',
-      alt: 'RTM Wiki Logo',
-    },
-    // tableOfContents
-    // editLink
-    editLink: {
-      baseUrl: 'https://github.com/Builder256/RTM-Wiki/tree/starlight',
-    },
-    sidebar: [
-      { label: 'トップ', link: '/' },
-      {
-        label: 'RealTrainMod入門',
-        items: [
-          { label: 'はじめに', link: 'getting-started/overview' },
-          { label: '初心者向け導入方法', link: 'getting-started/installing' },
-        ],
+  integrations: [
+    starlight({
+      title: 'RTM Wiki',
+      description:
+        '2025年に新しく設立されたRealTrainMod（RTM）のWiki、中級者以上のユーザーに向けた高度な情報を掲載',
+      logo: {
+        src: './src/assets/images/logos/wiki-logo.svg',
+        alt: 'RTM Wiki Logo',
       },
-      {
-        label: 'RealTrainModの使い方',
-        autogenerate: {
-          directory: 'usage/',
+      // tableOfContents
+      // editLink
+      editLink: {
+        baseUrl: 'https://github.com/Builder256/RTM-Wiki/tree/starlight',
+      },
+      sidebar: [
+        { label: 'トップ', link: '/' },
+        {
+          label: 'RealTrainMod入門',
+          items: [
+            { label: 'はじめに', link: 'getting-started/overview' },
+            { label: '初心者向け導入方法', link: 'getting-started/installing' },
+          ],
         },
-      },
-      {
-        label: 'アドオン制作',
-        items: [
-          { label: 'アドオン制作概要', link: 'dev/overview' },
-          {
-            label: 'ファイルとディレクトリ',
-            items: [
-              {
-                label: 'ディレクトリ構成とファイル名',
-                link: 'dev/file/directory-structure-and-file-name',
-              },
-              {
-                label: 'JSONファイル',
-                items: [
-                  {
-                    label: 'JSONファイルの概要',
-                    link: 'dev/file/json/overview',
-                  },
-                  {
-                    label: 'モデル設定用JSONファイル',
-                    link: 'dev/file/json/model',
-                  },
-                ],
-              },
-              {
-                label: 'スクリプト',
-                items: [
-                  {
-                    label: 'スクリプトの概要',
-                    link: 'dev/file/script/overview',
-                  },
-                ],
-              },
-            ],
+        {
+          label: 'RealTrainModの使い方',
+          autogenerate: {
+            directory: 'usage/',
           },
-          { label: '逆引きリファレンス', link: 'dev/reverse-reference' },
-          { label: 'トラブルシューティング', link: 'dev/troubleshooting' },
-          {
-            label: 'リファレンス',
-            items: [
-              {
-                label: 'JSON',
-                autogenerate: { directory: 'dev/reference/json' },
-              },
-            ],
-          },
-        ],
-      },
-      {
-        label: '関連Mod',
-        autogenerate: {
-          directory: 'mods/integrated',
         },
-      },
-      {
-        label: '外部Mod',
-        autogenerate: {
-          directory: 'mods/external',
-        },
-      },
-      {
-        label: 'RealTrainMod関連サイト',
-        items: [
-          {
-            label: 'realtrainmodWiki',
-            link: 'https://gamerch.com/realtrainmod/',
-            attrs: {
-              target: '_blank',
+        {
+          label: 'アドオン制作',
+          items: [
+            { label: 'アドオン制作概要', link: 'dev/overview' },
+            {
+              label: 'ファイルとディレクトリ',
+              items: [
+                {
+                  label: 'ディレクトリ構成とファイル名',
+                  link: 'dev/file/directory-structure-and-file-name',
+                },
+                {
+                  label: 'JSONファイル',
+                  items: [
+                    {
+                      label: 'JSONファイルの概要',
+                      link: 'dev/file/json/overview',
+                    },
+                    {
+                      label: 'モデル設定用JSONファイル',
+                      link: 'dev/file/json/model',
+                    },
+                  ],
+                },
+                {
+                  label: 'スクリプト',
+                  items: [
+                    {
+                      label: 'スクリプトの概要',
+                      link: 'dev/file/script/overview',
+                    },
+                  ],
+                },
+              ],
             },
-          },
-          {
-            label: 'RTM Addon Search',
-            link: 'https://rtmaddon-search.com/',
-            attrs: {
-              target: '_blank',
+            { label: '逆引きリファレンス', link: 'dev/reverse-reference' },
+            { label: 'トラブルシューティング', link: 'dev/troubleshooting' },
+            {
+              label: 'リファレンス',
+              items: [
+                {
+                  label: 'JSON',
+                  autogenerate: { directory: 'dev/reference/json' },
+                },
+              ],
             },
+          ],
+        },
+        {
+          label: '関連Mod',
+          autogenerate: {
+            directory: 'mods/integrated',
           },
-          {
-            label: 'RealTrainMod - CurseForge',
-            link: 'https://www.curseforge.com/minecraft/mc-mods/realtrainmod',
-            attrs: {
-              target: '_blank',
+        },
+        {
+          label: '外部Mod',
+          autogenerate: {
+            directory: 'mods/external',
+          },
+        },
+        {
+          label: 'RealTrainMod関連サイト',
+          items: [
+            {
+              label: 'realtrainmodWiki',
+              link: 'https://gamerch.com/realtrainmod/',
+              attrs: {
+                target: '_blank',
+              },
             },
+            {
+              label: 'RTM Addon Search',
+              link: 'https://rtmaddon-search.com/',
+              attrs: {
+                target: '_blank',
+              },
+            },
+            {
+              label: 'RealTrainMod - CurseForge',
+              link: 'https://www.curseforge.com/minecraft/mc-mods/realtrainmod',
+              attrs: {
+                target: '_blank',
+              },
+            },
+          ],
+        },
+        {
+          label: 'RTM Wikiを編集する',
+          collapsed: true,
+          autogenerate: {
+            directory: 'edit/',
           },
-        ],
-      },
-      {
-        label: 'RTM Wikiを編集する',
-        collapsed: true,
-        autogenerate: {
-          directory: 'edit/',
+        },
+      ],
+      defaultLocale: 'ja',
+      locales: {
+        ja: {
+          label: '日本語',
+          lang: 'ja',
+        },
+        en: {
+          label: 'English',
+          lang: 'en',
+        },
+        'zh-tw': {
+          label: '繁體中文',
+          lang: 'zh-TW',
         },
       },
-    ],
-    defaultLocale: 'ja',
-    locales: {
-      ja: {
-        label: '日本語',
-        lang: 'ja',
-      },
-      en: {
-        label: 'English',
-        lang: 'en',
-      },
-      'zh-tw': {
-        label: '繁體中文',
-        lang: 'zh-TW',
-      },
-    },
-    social: [
-      {
-        icon: 'github',
-        label: 'GitHub',
-        href: 'https://github.com/Builder256/RTM-Wiki',
-      },
-    ],
-    customCss: [
-      './src/styles/custom.css',
-      '@fontsource-variable/noto-sans-jp',
-    ],
-    // expressiveCode
-    // pagefind
-    // prerender
-    head: [
-      {
-        // <meta name="darkreader-lock">
-        tag: 'meta',
-        attrs: {
-          name: 'darkreader-lock',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/Builder256/RTM-Wiki',
         },
-      },
-      {
-        // <meta name="robots" content="noindex">
-        tag: 'meta',
-        attrs: {
-          name: 'robots',
-          content: 'noindex',
+      ],
+      customCss: [
+        './src/styles/custom.css',
+        '@fontsource-variable/noto-sans-jp',
+      ],
+      // expressiveCode
+      // pagefind
+      // prerender
+      head: [
+        {
+          // <meta name="darkreader-lock">
+          tag: 'meta',
+          attrs: {
+            name: 'darkreader-lock',
+          },
         },
+        {
+          // <meta name="robots" content="noindex">
+          tag: 'meta',
+          attrs: {
+            name: 'robots',
+            content: 'noindex',
+          },
+        },
+      ],
+      // lastUpdated
+      pagination: false,
+      favicon: '/images/favicon.svg',
+      // titleDelimiter
+      // disable404Route
+      // components
+      components: {
+        MarkdownContent: './src/components/CustomMarkdownContent.astro',
       },
-    ],
-    // lastUpdated
-    pagination: false,
-    favicon: '/images/favicon.svg',
-    // titleDelimiter
-    // disable404Route
-    // components
-    components: {
-      MarkdownContent: './src/components/CustomMarkdownContent.astro',
-    },
-    plugins: [starlightThemeGalaxy()],
-    // credits: true,
-  }), svelte()],
+      plugins: [starlightThemeGalaxy()],
+      // credits: true,
+    }),
+    svelte(),
+  ],
   markdown: {
     remarkPlugins: [
       remarkBreaks, // 改行をbrにする
