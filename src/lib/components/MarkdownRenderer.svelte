6d81ce1ae0f1562db5f47159ea949549339ebfd6
Builder <92662457+Builder256@users.noreply.github.com>
2026-02-11 17:47:41 +0900

    feat: svelte-exmarkdownを使用したMarkdownのレンダリングコンポーネントを実装

---
+++ src/lib/components/MarkdownRenderer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Markdown, { type Plugin } from 'svelte-exmarkdown';
+
+  import remarkDirective from 'remark-directive';
+  import remarkBreaks from 'remark-breaks';
+
+  interface Props {
+    content: string;
+  }
+
+  const { content: md }: Props = $props();
+
+  const plugins: Plugin[] = [{ remarkPlugin: remarkDirective }, { remarkPlugin: remarkBreaks }];
+</script>
+
+<!-- 日本語のtypographyについて: https://gist.github.com/tak-dcxi/0f8b924d6dd81aaeb58dc2e287f2ab3a -->
+<div data-slot="markdown-renderer" class="space-y-4 wrap-anywhere [line-break:strict]" data-md-content>
+  <Markdown {md} {plugins} />
+</div>
