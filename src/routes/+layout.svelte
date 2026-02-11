e7916cca07881b0de2fc98aa3ee1bf754838dd5e
Builder <92662457+Builder256@users.noreply.github.com>
2026-02-11 23:01:04 +0900

    feat: 基本レイアウト、ルートページ、日本語翻訳を追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,12 @@
   import { locales, localizeHref } from '$lib/paraglide/runtime';
   import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
+  import { m } from '$lib/paraglide/messages';
+  import Container from '$lib/components/Container.svelte';
 
   let { children } = $props();
+
+  const SITE_NAME = 'RTM Wiki';
 </script>
 
 <svelte:head>
@@ -13,12 +17,72 @@
   <meta name="darkreader-lock" />
 </svelte:head>
 
-<header class="flex h-18 items-center border-b border-border">
-  <a href="/" class="">RTM Wiki</a>
+<header class="contents">
+  <!-- ヘッダ1段目 -->
+  <Container>
+    <div class="flex h-12 items-center justify-between">
+      <a href="/" class="mx-2 text-xl transition-colors duration-300 hover:text-accent">{SITE_NAME}</a>
+      <div class="flex">
+        <a href="edit/" class="mx-2 transition-colors duration-300 hover:text-accent">{m.header_edit()}</a>
+        <button class="mx-2 transition-colors duration-300 hover:text-accent">
+          {m.header_menu()}
+        </button>
+      </div>
+    </div>
+  </Container>
+  <!-- ヘッダ2段目 -->
+  <div class="sticky top-0 border-b border-border/80 bg-background/50 backdrop-blur-lg">
+    <Container>
+      <div class="flex h-12 items-center justify-between">
+        <button class="mx-2">目次</button>
+
+        <div class="mx-auto">ページタイトル</div>
+      </div>
+    </Container>
+  </div>
 </header>
-<main>
-  {@render children()}
-</main>
+
+<Container>
+  <div class="flex">
+    <nav></nav>
+
+    <main class="px-2 py-8">
+      {@render children()}
+    </main>
+
+    <div></div>
+  </div>
+</Container>
+
+<footer class="border-t border-border/80">
+  <Container>
+    <div class="space-y-2 px-2 py-8">
+      <p>MIT License</p>
+      <p>
+        Copyright &copy; {new Date().getFullYear()}
+        {SITE_NAME}
+      </p>
+
+      <p>
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+        the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+        to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      </p>
+      <p>
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+        the Software.
+      </p>
+      <p>
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+        THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+        CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+        DEALINGS IN THE SOFTWARE.
+      </p>
+    </div>
+  </Container>
+</footer>
 
 <div style="display:none">
   {#each locales as locale}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h1>Welcome to RTM Wiki</h1>
+<script lang="ts">
+  import MarkdownRenderer from '$lib/components/MarkdownRenderer.svelte';
+  import { parseMarkdown } from '$lib/utils/markdown';
+
+  interface Contents {
+    [key: string]: string;
+  }
+
+  /**
+   * `/content/`内のmdファイル
+   * ```typescript
+   * {
+   *   'ルート相対パス': 'ファイルの内容',
+   *   '/content/foo.md': 'foo.mdの内容',
+   * }
+   * ```
+   */
+  const allContent: Contents = import.meta.glob('/content/**/*.md', { eager: true, query: '?raw', import: 'default' });
+
+  const rawContent = allContent['/content/ja/index.md'];
+  const { content, metadata } = parseMarkdown(rawContent);
+
+  console.log(metadata);
+</script>
+
+<MarkdownRenderer {content} />
