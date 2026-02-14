885d391677c1e58c9a6162452028f95d841486d6
Builder <92662457+Builder256@users.noreply.github.com>
2026-02-15 04:09:41 +0900

    refactor: 余分なスタイルを削除

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -2,4 +2,4 @@
   let { children } = $props();
 </script>
 
-<div data-slot="container" class="mx-auto max-w-7xl border-border">{@render children()}</div>
+<div data-slot="container" class="mx-auto max-w-7xl">{@render children()}</div>
