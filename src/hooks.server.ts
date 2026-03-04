bcdf4222f05279596b93d1945911e1ed273c0a76
Builder <92662457+Builder256@users.noreply.github.com>
2026-03-04 20:04:45 +0900

    feat: lang属性の付与を実装

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,12 @@
-// paradlideの仕様を理解できていないので介入させないためにコメントアウト
-// import type { Handle } from '@sveltejs/kit';
-// import { paraglideMiddleware } from '$lib/paraglide/server';
+import type { Handle } from '@sveltejs/kit';
+import { baseLocale, isLocale } from '$lib/paraglide/runtime';
 
-// const handleParaglide: Handle = ({ event, resolve }) =>
-// 	paraglideMiddleware(event.request, ({ request, locale }) => {
-// 		event.request = request;
-
-// 		return resolve(event, {
-// 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
-// 		});
-// 	});
-
-// export const handle: Handle = handleParaglide;
+export const handle: Handle = ({ event, resolve }) => {
+  /** URLの最初のパス */
+  const firstPath = event.url.pathname.split('/')[1];
+  /** 判定した現在のロケール */
+  const currentLocale = isLocale(firstPath) ? firstPath : baseLocale;
+  return resolve(event, {
+    transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', currentLocale),
+  });
+};
